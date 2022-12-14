59a726efccef5cb8f01107099103d754aef0b68e
zachary <kauffzach@gmail.com>
2022-12-14 14:20:37 -0500

    backend express setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('hello/world', (req, res) => {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World');
+// Add an XSRF-TOKEN cookie
+router.get('/api/csrf/restore', (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie('XSRF-TOKEN', csrfToken);
+    res.status(200).json({
+        'CSRF-Token': csrfToken
+    });
 });
 
 module.exports = router;
