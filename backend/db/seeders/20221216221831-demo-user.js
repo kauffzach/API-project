0561e457d24316c1e80c09aeab63c251c431c64d
zachary <kauffzach@gmail.com>
2022-12-16 17:26:48 -0500

    phase 3 user authentification

---
+++ backend/db/seeders/20221216221831-demo-user.js
@@ -0,0 +1,53 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+const { options } = require('../../routes');
+
+if(process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   options.tableName = 'Users';
+
+   await queryInterface.bulkInsert(options, [
+    {
+      email: 'test@user.io',
+      username: 'test-user',
+      hashedPassword: bcrypt.hashSync('password')
+    },
+    {
+      email: 'test2@user.io',
+      username: 'bozo123',
+      hashedPassword: bcrypt.hashSync('password2')
+    }
+   ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(options, {
+      username: {
+        [Op.in]: ['test-user', 'bozo123']
+      }
+    }, {});
+  }
+};
