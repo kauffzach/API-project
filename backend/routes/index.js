012bbc7e811d428ef7cd8ff81647ba8b0cbb73df
zachary <kauffzach@gmail.com>
2022-12-16 16:52:16 -0500

    phase 1 api route completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api')
+
+
+router.use('/api', apiRouter)
 
 // Add an XSRF-TOKEN cookie
 router.get('/api/csrf/restore', (req, res) => {
