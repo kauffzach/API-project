92cc9e2675a41aa07efd4ce48f21810483e06814
zachary <kauffzach@gmail.com>
2022-12-19 21:21:48 -0500

    create bookings table

---
+++ backend/db/models/bookings.js
@@ -0,0 +1,54 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class bookings extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  bookings.init({
+    startDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isDate: true,
+        isBefore: this.endDate,
+      }
+    },
+    endDate: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isDate: true,
+        isAfter: this.startDate,
+        endDateAfterStartDate(value) {
+          const startDate = new Date(this.startDate);
+          const endDate = new Date(value);
+
+          if (endDate <= startDate) {
+            throw new Error('endDate cannot come before startDate');
+          };
+        },
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'bookings',
+  });
+  return bookings;
+};
