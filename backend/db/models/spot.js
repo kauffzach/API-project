8f647f304b333c6c05f67aefefc3f256352c6f54
zachary <kauffzach@gmail.com>
2022-12-19 21:26:34 -0500

    added reviews, reviewImages, spotImages tables

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class spots extends Model {
+  class Spot extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  spots.init({
+  Spot.init({
     ownerId: {
       type: DataTypes.INTEGER
     },
@@ -65,7 +65,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }, {
     sequelize,
-    modelName: 'spots',
+    modelName: 'Spot',
   });
-  return spots;
+  return Spot;
 };
